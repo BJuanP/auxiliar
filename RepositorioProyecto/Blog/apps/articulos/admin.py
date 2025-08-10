fecdecf6ad0ad2b7a82ff86964aeb06305724c17
Juan Pablo Bravo <114406263+BJuanP@users.noreply.github.com>
2025-08-10 12:53:27 -0300

    Add files via upload

---
+++ RepositorioProyecto/Blog/apps/articulos/admin.py
@@ -0,0 +1,6 @@
+
+from django.contrib import admin
+
+from .models import Articulo
+
+admin.site.register(Articulo)   #Esto es para traer al admin la clase Producto
---
+++ RepositorioProyecto/Blog/apps/articulos/views.py
@@ -0,0 +1,36 @@
+
+from django.shortcuts import render
+from django.views.generic.detail import DetailView    #Esto es una vista preparada para mostrar el detalle de un proyecto
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+from .models import Articulo            #Esto es necesario para traer lo que está en la clase Producto
+from .forms import FormularioCrearArticulo
+
+
+#VISTA BASADA EN FUNCIONES
+def Listar_Articulos(request):
+    
+    #ORM = SELECT * FROM PRODUCTO
+    todos = Articulo.objects.all()    #Acá le digo que me traiga todos los objetos de la tabla productos
+    
+
+    return render(request, 'articulos/listar.html', {'articulos': todos})       #Esto es para pasarle todos los productos al template. Es una lista
+
+
+
+#VISTA BASADA EN CLASES
+class Detalle_Articulo(DetailView):
+    
+    template_name = 'articulos/detalle.html'
+    model = Articulo
+    context_object_name = 'articulo'
+
+
+
+class Crear_Articulo(CreateView):     
+    
+    model = Articulo
+    template_name = 'articulos/crear.html'
+    form_class = FormularioCrearArticulo
+    success_url = reverse_lazy('articulos:path_listar_articulos')
